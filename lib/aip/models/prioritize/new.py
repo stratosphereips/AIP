731024d014e00f995ab80e2e8f44abd1ae86948c
joaquinbogado <joaquinbogado@gmail.com>
2022-03-15 10:18:23 -0300

    Added base code for prioritizers

---
+++ lib/aip/models/prioritize/new.py
@@ -0,0 +1,37 @@
+""" 
+AIP - Prioritize New Algorithm
+
+This module prioritize the new attackers.
+
+This program is free software: you can redistribute it and/or modify it under
+the terms of the GNU General Public License as published by the Free Software
+Foundation, either version 3 of the License, or (at your option) any later
+version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+You should have received a copy of the GNU General Public License along with
+this program. If not, see <http://www.gnu.org/licenses/>.
+"""
+
+__authors__ = ["Joaquin Bogado <joaquinbogado@duck.com>"]
+__contact__ = "stratosphere@aic.fel.cvut.cz"
+__copyright__ = "Copyright 2022, Stratosphere Laboratory."
+__credits__ = ["Joaquín Bogado"]
+__deprecated__ = False
+__license__ = "GPLv3"
+__maintainer__ = "Joaquin Bogado"
+__version__ = "0.0.1"
+
+
+from aip.models.base import BaseModel
+
+class PrioritizeNew(BaseModel):
+    '''
+    Prioritize New algorithm
+    '''
+    
+    def __init__(self, lookback=1):
+        super().__init__()
+        self.lookback = lookback
