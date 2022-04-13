383d6c0c4c03a67308484f2f92c63328cf59a3b4
joaquinbogado <joaquinbogado@gmail.com>
2022-04-13 16:26:01 -0300

    Added Prioritize New (PN) model

--- lib/aip/models/prioritize/new.py
+++ lib/aip/models/prioritize/new.py
@@ -24,14 +24,49 @@ __license__ = "GPLv3"
 __maintainer__ = "Joaquin Bogado"
 __version__ = "0.0.1"
 
+import numpy as np
+import pandas as pd
 
 from aip.models.base import BaseModel
+from aip.models.prioritize.knowledge import Knowledgebase
+from datetime import date
 
 class PrioritizeNew(BaseModel):
     '''
     Prioritize New algorithm
     '''
     
-    def __init__(self, lookback=1):
+    def __init__(self):
         super().__init__()
-        self.lookback = lookback
+        self.db = Knowledgebase()
+        # Weights from Thomas' Thesis
+        # nflows, conns, nbytes, npackets,
+        # mean_flows, mean_conns, mean_bytes, mean_packets
+        self.weights = [0.2, 0.2, 0.2, 0.2, 0.05, 0.05, 0.05, 0.05]
+
+    def _get_IP_scores(self):
+        days_since_last_seen = np.array([x.days for x in (pd.to_datetime(date.today()) - self.db.knowledge.last_seen)])
+        features = self.db.knowledge[
+                ['flows', 'duration','bytes', 'packets', 'mean_flows',
+                    'mean_duration', 'mean_bytes', 'mean_packets']
+                ].values
+        normalized_features = np.zeros_like(features)
+        for i in range(8):
+            normalized_features[:,i] = (features[:,i] - features[:,i].min()) / (features[:,i].max() - features[:,i].min())
+        ipscores = features * self.weights
+        ipscores = ipscores.sum(axis=1)
+        aging = 2 / (2 + days_since_last_seen)
+        ipscores *= aging
+        return ipscores
+
+    def run(self, for_date=date.today()):
+        ipscores = self._get_IP_scores()
+        df = pd.DataFrame()
+        df['ip'] = self.db.knowledge['orig'].values
+        df['score'] = ipscores
+        df = df.sort_values(by='score')
+        return df
+
+
+
+        
