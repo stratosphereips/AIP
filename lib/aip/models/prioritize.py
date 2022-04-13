5cc6ee8798707f418cfae100c0c744a0b6dcdb3f
joaquinbogado <joaquinbogado@gmail.com>
2022-04-13 16:50:20 -0300

    Refactoring of the prioritizers

--- lib/aip/models/prioritize.py
+++ lib/aip/models/prioritize.py
@@ -1,7 +1,7 @@
 """ 
-AIP - Database module for Prioritize New and Prioritize Consistent
+AIP - Prioritize New/Consistent Algorithm
 
-This module implements the Database Abstraction from Thomas O'Hara thesis
+This module prioritize the new/consistent attackers.
 
 This program is free software: you can redistribute it and/or modify it under
 the terms of the GNU General Public License as published by the Free Software
@@ -24,13 +24,15 @@ __license__ = "GPLv3"
 __maintainer__ = "Joaquin Bogado"
 __version__ = "0.0.1"
 
-import pandas as pd
 
+import numpy as np
+import pandas as pd
+  
 from aip.data.access import data_path, get_attacks
 from aip.models.base import BaseModel
+#from aip.models.prioritize import Knowledgebase
 from datetime import date, timedelta
 from os import path
-
 
 
 class Knowledgebase():
@@ -117,3 +119,81 @@ class Knowledgebase():
         # FIXME: Need to code the update function so models don't worry about knowledge updates
         # Knowledge should be always updated upon Knowledge instantiation?
         pass
+
+
+class Consistent(BaseModel):
+    '''
+    Prioritize Consistent algorithm
+    '''
+
+    def __init__(self):
+        super().__init__()
+        self.db = Knowledgebase()
+        # Weights from Thomas' Thesis
+        # nflows, conns, nbytes, npackets,
+        # mean_flows, mean_conns, mean_bytes, mean_packets
+        self.weights = [0.05, 0.05, 0.05, 0.05, 0.2, 0.2, 0.2, 0.2]
+        self.score_threshold = 0.002
+
+    def _get_IP_scores(self):
+        days_since_last_seen = np.array([x.days for x in (pd.to_datetime(date.today()) - self.db.knowledge.last_seen)])
+        features = self.db.knowledge[
+                ['flows', 'duration','bytes', 'packets', 'mean_flows',
+                    'mean_duration', 'mean_bytes', 'mean_packets']
+                ].values
+        normalized_features = np.zeros_like(features)
+        for i in range(8):
+            normalized_features[:,i] = (features[:,i] - features[:,i].min()) / (features[:,i].max() - features[:,i].min())
+        ipscores = features * self.weights
+        ipscores = ipscores.sum(axis=1)
+        days_since_last_seen = np.array([x.days for x in (pd.to_datetime(date.today()) - self.db.knowledge.last_seen)])
+        total_attack_time = np.array([x.days for x in (self.db.knowledge.last_seen - self.db.knowledge.first_seen)])
+        aging = 1 - (days_since_last_seen/(days_since_last_seen + total_attack_time))
+        ipscores *= aging
+        return ipscores
+
+    def run(self, for_date=date.today()):
+        ipscores = self._get_IP_scores()
+        df = pd.DataFrame()
+        df['ip'] = self.db.knowledge['orig'].values
+        df['score'] = ipscores
+        df = df.sort_values(by='score', ascending=False)
+        return df[df.score > self.score_threshold]
+
+
+class New(BaseModel):
+    '''
+    Prioritize New algorithm
+    '''
+
+    def __init__(self):
+        super().__init__()
+        self.db = Knowledgebase()
+        # Weights from Thomas' Thesis
+        # nflows, conns, nbytes, npackets,
+        # mean_flows, mean_conns, mean_bytes, mean_packets
+        self.weights = [0.2, 0.2, 0.2, 0.2, 0.05, 0.05, 0.05, 0.05]
+        self.score_threshold = 0.002
+
+    def _get_IP_scores(self):
+        days_since_last_seen = np.array([x.days for x in (pd.to_datetime(date.today()) - self.db.knowledge.last_seen)])
+        features = self.db.knowledge[
+                ['flows', 'duration','bytes', 'packets', 'mean_flows',
+                    'mean_duration', 'mean_bytes', 'mean_packets']
+                ].values
+        normalized_features = np.zeros_like(features)
+        for i in range(8):
+            normalized_features[:,i] = (features[:,i] - features[:,i].min()) / (features[:,i].max() - features[:,i].min())
+        ipscores = features * self.weights
+        ipscores = ipscores.sum(axis=1)
+        aging = 2 / (2 + days_since_last_seen)
+        ipscores *= aging
+        return ipscores
+
+    def run(self, for_date=date.today()):
+        ipscores = self._get_IP_scores()
+        df = pd.DataFrame()
+        df['ip'] = self.db.knowledge['orig'].values
+        df['score'] = ipscores
+        df = df.sort_values(by='score', ascending=False)
+        return df[df.score > self.score_threshold]
