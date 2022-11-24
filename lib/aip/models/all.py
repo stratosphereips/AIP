7b5c815b7bb27b3198ccabf2c889d7f9187e3efd
joaquinbogado <joaquinbogado@gmail.com>
2022-11-24 13:13:03 +0100

    Added model that remembers all attackers IPs

---
+++ lib/aip/models/all.py
@@ -0,0 +1,52 @@
+""" 
+AIP - AllIPs Model
+
+This model filter the attackers of the previous X days. 
+
+This program is free software: you can redistribute it and/or modify it under
+the terms of the GNU General Public License as published by the Free Software
+Foundation, either version 3 of the License, or (at your option) any later
+version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+You should have received a copy of the GNU General Public License along with
+this program. If not, see <http://www.gnu.org/licenses/>.
+"""
+
+__authors__ = ["Joaquin Bogado <joaquinbogado@duck.com>"]
+__contact__ = "stratosphere@aic.fel.cvut.cz"
+__copyright__ = "Copyright 2022, Stratosphere Laboratory."
+__credits__ = ["Joaquín Bogado"]
+__deprecated__ = False
+__license__ = "GPLv3"
+__maintainer__ = "Joaquin Bogado"
+__version__ = "0.0.1"
+
+import pandas as pd
+
+from aip.models.base import BaseModel
+from aip.utils.autoload import register, models
+from aip.data.access import get_attacks
+from datetime import date, timedelta
+
+#__models__ = []
+
+@register
+class AllIPs(BaseModel):
+    def __init__(self, lookback=1):
+        super().__init__()
+        self.lookback = lookback
+
+    def run(self, for_date=date.today()):
+        start = '2020-07-05'
+        end = str(for_date - timedelta(days=1))
+        # get all the attackers IPs
+        attacks = get_attacks(start, end, usecols=['orig'])
+        attacks = pd.concat(attacks)
+        attacks = attacks.rename(columns={'orig':'ip'})
+        self.blocklist = attacks.drop_duplicates()
+        self.sanitize()
+        return self.blocklist
+
