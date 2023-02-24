352acf24642ca4dc95951f2f03557934205d17a6
joaquinbogado <joaquinbogado@gmail.com>
2023-02-24 14:30:52 +0100

    Unified prioritizers models with production in main

--- lib/aip/models/prioritize.py
+++ lib/aip/models/prioritize.py
@@ -44,6 +44,11 @@ def _add_knowledge(last_knowledge, day):
     attacks = get_attacks(dates=[day])
     df = attacks[0]
     if df.empty is True:
+        last_knowledge.to_csv(path.join(p, f'knowledgebase-{day}-snapshot.gz'),
+            columns=['orig', 'flows', 'duration','bytes', 'packets',
+                'mean_flows', 'mean_duration', 'mean_bytes', 'mean_packets',
+                'days_active', 'first_seen', 'last_seen'],
+            index=False, compression='gzip')
         return last_knowledge
     df = df.rename(columns={"count": "flows"})
     df.loc[:, 'first_seen'] = day
@@ -252,7 +257,7 @@ class RandomForest(BaseModel):
             df = target.features[pred.astype(bool)]
             df = df.rename(columns={'orig': 'ip'})
             df = self.sanitize(df)
-            return df['ip']
+            return df[['ip']]
         else:
-            df = pd.DataFrame(columns=['ip'])
+            df = pd.DataFrame(columns=[['ip']])
             return df
