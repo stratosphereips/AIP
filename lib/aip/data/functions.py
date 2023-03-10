79d7875c819a48aa53148cb210daa95d1376f53b
joaquinbogado <joaquinbogado@gmail.com>
2023-03-10 09:46:03 +0100

    Syncked with main

--- lib/aip/data/functions.py
+++ lib/aip/data/functions.py
@@ -4,53 +4,26 @@ import hashlib
 import pandas as pd
 import shutil
 import subprocess, shlex
+import zeeklog2pandas as z2p
 
 from dotenv import dotenv_values
 from joblib import Parallel, delayed
+from os import makedirs, path, access, W_OK
 from pathlib import Path
-from os import makedirs, path, scandir
 
 _project_dir = Path(__file__).resolve().parents[3]
 _config = {
     **dotenv_values(path.join(_project_dir, ".env")),  # load sensitive variables
 }
 
-class NotAZeekFile(Exception):
-    pass
-
-def read_zeek_header(path):
-    header = dict()
-    try:
-        f = gzip.open(path, 'rt')
-        line = f.readline()
-    except gzip.BadGzipFile:
-        f = open(path, 'rt')
-        line = f.readline()
-    if '#separator' not in line:
-        raise NotAZeekFile
-    header['separator'] = line.rstrip().split()[-1].encode().decode('unicode_escape')
-    line = f.readline()
-    header['set_separator'] = line.rstrip().split()[-1]
-    line = f.readline()
-    header['empty_field'] = line.rstrip().split()[-1]
-    line = f.readline()
-    header['unset_field'] = line.rstrip().split()[-1]
-    line = f.readline()
-    header['path'] = line.rstrip().split()[-1]
-    line = f.readline()
-    header['open'] = dt.datetime.strptime(str(line.rstrip().split()[-1]),"%Y-%m-%d-%H-%M-%S")
-    line = f.readline()
-    header['fields'] = line.rstrip().split()[1:]
-    line = f.readline()
-    header['types'] = line.rstrip().split()[1:]
-    return header
-
 def read_zeek(path, **kwargs):
-    header = read_zeek_header(path)
-    df = pd.read_csv(path, skiprows=8, names=header['fields'], sep=header['separator'], comment='#', **kwargs)
-    if 'ts' in df.keys():
-        df['ts'] = pd.to_datetime(df.ts, unit='s')
-    return df
+    try:
+        df = z2p.read_zeek(path, **kwargs)
+        if 'ts' in df.keys():
+            df['ts'] = pd.to_datetime(df.ts, unit='s')
+        return df
+    except:
+        raise z2p.NotAZeekFile(path)
 
 def read_argus(path, **kwargs):
     from os import path as ospath
@@ -73,14 +46,9 @@ def scramble(s):
 def getrawdata(date):
     dt.datetime.strptime(date, '%Y-%m-%d')
     p = path.join(_project_dir,'data','raw', date)
-    makedirs(p, exist_ok=True)
-    commands = [shlex.split(_config['magic'] + f'{date}/conn.{x:02}* ' + p) for x in range(0,24)]
-    Parallel(n_jobs=24, backend='threading')(delayed(subprocess.run)(c) for c in commands)
-    # Only retrieve argus if zeek files aren't available
-    if len(list(scandir(p))) == 0:
-        # conn logs not found, retrieving argus files
-        year, month, date = str(dt.datetime.strptime(date, '%Y-%m-%d').date()).split('-')
-        commands = [shlex.split(_config['magic_argus'] + f'{year}/{month}/{date}/*{x:02}.*.biargus.xz ' + p) for x in range(0,24)]
+    if access(p, W_OK):
+        makedirs(p, exist_ok=True)
+        commands = [shlex.split(_config['magic'] + f'{date}/conn.{x:02}* ' + p) for x in range(0,24)]
         Parallel(n_jobs=24, backend='threading')(delayed(subprocess.run)(c) for c in commands)
 
 def removerawdata(date, force=False):
