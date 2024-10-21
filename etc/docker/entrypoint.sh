7530bafe0c7aa4f09c9a8438ae7a00ab15abb1ef
verovaleros <vero.valeros@gmail.com>
2024-10-21 18:29:36 +0000

    Explain entrypoint.sh functionality

--- etc/docker/entrypoint.sh
+++ etc/docker/entrypoint.sh
@@ -1,8 +1,16 @@
 #!/bin/bash --login
+
+# Exit immediately if any command exits with a non-zero status
 set -e
 
+# Force the command prompt to display colors
+export force_color_prompt=yes
+
+# Initialize Conda in the current shell session
 source $HOME/miniconda3/etc/profile.d/conda.sh
 
+# Activate the conda environment already created in the docker
 conda activate aip
-export force_color_prompt=yes
+
+# Execute any command passed to the container when run
 exec "$@"
