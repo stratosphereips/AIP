bc011160b7cf7853d7488f5e21f0f6cd98f3294f
Veronica Valeros <vero.valeros@gmail.com>
2024-11-03 12:43:45 +0100

    Add validation future date (#65)
    * Exit on invalid date
    
    * Raise exception on future date
    
    * Add test for checking future date raise exception

--- lib/aip/utils/date_utils.py
+++ lib/aip/utils/date_utils.py
@@ -1,5 +1,5 @@
-import logging
 from datetime import datetime
+from datetime import date
 
 
 def validate_and_convert_date(date_str):
@@ -7,8 +7,10 @@ def validate_and_convert_date(date_str):
     Validates a date string in 'YYYY-MM-DD' format and converts it to a date object.
     """
     try:
-        dateobj = datetime.strptime(date_str, '%Y-%m-%d')
-        return dateobj.date()
-    except ValueError as e:
-        logging.error(f"Invalid date format for '{date_str}', expected YYYY-MM-DD")
-        raise ValueError(f"Invalid date format: {date_str}, expected YYYY-MM-DD") from e
+        dateobj = datetime.strptime(date_str, '%Y-%m-%d').date()
+        date_today = date.today()
+        if dateobj > date_today:
+            raise ValueError(f"The input date '{date_str}' cannot be in the future.")
+        return dateobj
+    except ValueError as err:
+        raise ValueError(f"Invalid date format: {date_str}, expected YYYY-MM-DD") from err
