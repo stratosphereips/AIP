e7baf3944c13fd369906d095b1674b706c48bdfb
verovaleros <vero.valeros@gmail.com>
2024-11-13 13:10:45 +0000

    Document getrawdata()

--- lib/aip/data/functions.py
+++ lib/aip/data/functions.py
@@ -21,6 +21,7 @@ _config = {
     **dotenv_values(path.join(_project_dir, ".env")),  # load sensitive variables
 }
 
+
 def read_zeek(path, **kwargs):
     """
     Reads a Zeek file to a DataFrame
@@ -39,6 +40,7 @@ def read_zeek(path, **kwargs):
         raise z2p.NotAZeekFile(path)
 
 
+# Currently deprecated, AIP is using now strictly Zeek logs
 def read_argus(path, **kwargs):
     from os import path as ospath
     if ospath.exists(path.path + '.csv'):
@@ -54,16 +56,39 @@ def read_argus(path, **kwargs):
     df.rename(columns={'StartTime':'ts', 'SrcAddr':'id.orig_h', 'DstAddr':'id.resp_h', 'Dur': 'duration', 'SrcBytes': 'orig_ip_bytes', 'SrcPkts': 'orig_pkts'}, inplace=True)
     return df
 
+
+# This function is unused right now
 def scramble(s):
     return hashlib.sha1(_config['salt'].encode() + s.encode()).hexdigest()
 
+
 def getrawdata(date):
+    """
+    Retrieves Zeek data from a remote? location and stores it
+    on a directory for AIP to process it. The copy is done in
+    parallel.
+    """
+
+    # Validate date is well formatted
     dt.datetime.strptime(date, '%Y-%m-%d')
-    p = path.join(_project_dir,'data','raw', date)
-    if access(p, W_OK):
-        makedirs(p, exist_ok=True)
-        commands = [shlex.split(_config['magic'] + f'{date}/conn.{x:02}* ' + p) for x in range(0,24)]
-        Parallel(n_jobs=24, backend='threading')(delayed(subprocess.run)(c) for c in commands)
+
+    raw_data_dir = path.join(_project_dir,'data','raw', date)
+
+    # Ensure directory exists and is writable
+    if access(raw_data_dir, W_OK):
+        # Create directory, ignore if it exists
+        makedirs(raw_data_dir, exist_ok=True)
+
+        # The next part seems to be prepared to retrieve data from a location
+        # and store it in the data/raw/YYYY-MM-DD directory for processing.
+        commands = [
+            shlex.split(_config['magic'] + f'{date}/conn.{x:02}* ' + raw_data_dir) 
+            for x in range(0,24)
+        ]
+
+        # Attempting to run the previous commands in parallel
+        Parallel(n_jobs=24, backend='threading')(delayed(subprocess.run)(cmd) for cmd in commands)
+
 
 def removerawdata(date, force=False):
     dt.datetime.strptime(date, '%Y-%m-%d')
@@ -71,4 +96,3 @@ def removerawdata(date, force=False):
     # Only delete raw data if explicitly allowed in the configuration file
     if (_config['remove_raw_data'].lower() == 'true') or force:
         shutil.rmtree(p)
-
